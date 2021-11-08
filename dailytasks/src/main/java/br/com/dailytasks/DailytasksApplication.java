c21c53ea41f239c440edb6d52ed4efc8635e5bd3
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-08 17:55:15 -0300

    [Feature Isaias - Task: primeiro commit - implementacao das entidades, atributos e getters and setters]

---
+++ dailytasks/src/main/java/br/com/dailytasks/DailytasksApplication.java
@@ -0,0 +1,13 @@
+package br.com.dailytasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailytasksApplication.class, args);
+	}
+
+}
