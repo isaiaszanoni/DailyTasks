ec2a504084ecdd90958f6c00407543934c051239
Bruno Luna <luna.brsilva@gmail.com>
2021-11-09 15:25:32 -0300

    [Feature Luna - Task: Inserção da dependencia security e criação do método findByEmail]

--- dailytasks/src/main/java/br/com/dailytasks/repository/UsuarioRepository.java
+++ dailytasks/src/main/java/br/com/dailytasks/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.dailytasks.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,9 @@ import br.com.dailytasks.models.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	
+	
+	
+	Optional<Usuario> findByEmail(String email);
 	
 }
