29985747c7856bc87cb96eaadbbc1c1eb8f4cd29
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-12 11:34:35 -0300

    [Feature: Isaias - Task: Criacao dos componentes Home e Sobre]

--- dailytasks - front/dailytasks/src/app/app.module.ts
+++ dailytasks - front/dailytasks/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
