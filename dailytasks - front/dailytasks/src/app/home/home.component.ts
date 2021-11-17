b1ca30dac4378bb11139b4720ec332a1b9f0c6ee
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-17 13:25:00 -0300

    [Feature Isaias - Task: Funcionalidade dos modais de cadast login]

--- dailytasks - front/dailytasks/src/app/home/home.component.ts
+++ dailytasks - front/dailytasks/src/app/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Usuario } from '../model/Usuario';
-import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,48 +7,10 @@ import { AuthService } from '../service/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  user: Usuario = new Usuario // para Login e para Cadastro
-
-  // armazena o valor do input de senha para gerar evento
-  confirmarSenha: string
-
   constructor(
-    private authService: AuthService,
-    private router: Router
   ) { }
 
   ngOnInit(){
     window.scroll(0,0)
   }
-
-  confirmSenha(event: any) {
-    this.confirmarSenha = event.target.value // !!!!!!!!!!!
-  }
-
-  cadastrar() {
-    
-    if(this.user.senha != this.confirmarSenha){
-      alert('As senhas estão incorretas!')
-    }else {
-      this.authService.register(this.user).subscribe((resp: Usuario) => {
-        this.user = resp
-        this.router.navigate(['/home'])
-        alert('Usuário cadastrado com sucesso!')
-         
-      })
-
-    }
-  }
-
-  entrar() {
-    this.authService.login(this.user).subscribe((resp: Usuario) =>{
-      this.user = resp
-      this.router.navigate(['/tasks'])
-    }, err =>{
-      if(err.status == 500){
-        alert('Usuário ou senha incorretos!')
-      }
-    })
-  }
-
 }
--- dailytasks - front/dailytasks/src/app/navbar/navbar.component.ts
+++ dailytasks - front/dailytasks/src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  // para Login e para Cadastro
+  user: Usuario = new Usuario 
 
-  ngOnInit(): void {
+  // armazena o valor do input de senha para gerar evento
+  confirmarSenha: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value 
+  }
+
+  cadastrar() {
+    
+    if(this.user.senha != this.confirmarSenha){
+      alert('As senhas não correspondem!')
+    }else {
+      this.authService.register(this.user).subscribe((resp: Usuario) => {
+        this.user = resp
+        this.router.navigate(['/home'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
+  }
+
+  entrar() {
+    this.authService.login(this.user).subscribe((resp: Usuario) =>{
+      this.user = resp
+      this.router.navigate(['/tasks'])
+    }, err =>{
+      if(err.status == 500){
+        alert('Usuário ou senha incorretos!')
+      }
+    })
   }
 
 }
