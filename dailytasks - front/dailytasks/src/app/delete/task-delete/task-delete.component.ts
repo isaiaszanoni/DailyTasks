597aaca3643ab718d3a1ef0b18df67220dc19dde
isaiaszanoni <isaiasz.mus@gmail.com>
2021-11-19 18:21:38 -0300

    [Feature Isaias - Task: Implementacao do metodo deletar tasks, icones do card da task e outros detalhes]

--- dailytasks - front/dailytasks/src/app/delete/task-delete/task-delete.component.ts
+++ dailytasks - front/dailytasks/src/app/delete/task-delete/task-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tarefas } from 'src/app/model/Tarefas';
+import { TaskService } from 'src/app/service/task.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-task-delete',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskDeleteComponent implements OnInit {
 
-  constructor() { }
+  task: Tarefas = new Tarefas()
+  idtask: number
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit() {
+    if (environment.token == '') {
+      this.router.navigate(['/home'])
+    }
+
+    this.idtask = this.route.snapshot.params['id']
+    this.findTaskById(this.idtask)// o metodo pode estar errado
+    
   }
+
+  findTaskById(id: number) {
+    this.taskService.getTaskById(id).subscribe((resp: Tarefas) =>{
+      this.task = resp
+    })
+  }
+
+  apagar() {
+   this.taskService.deleteTask(this.idtask).subscribe(()=>{
+     alert('Postagem apagada com sucesso!')
+     this.router.navigate(['/tasks'])
+   })
+  }
+
 
 }
