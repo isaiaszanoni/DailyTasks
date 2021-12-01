2b633b2318d8cc5fa9b9ee93c65e6500beb71d9f
isaiaszanoni <isaiasz.mus@gmail.com>
2021-12-01 14:58:28 -0300

    [Feature Isaias - Task: adicao os atributos Cor e Finalizada a entidade Tarefa]

--- dailytasks/src/main/java/br/com/dailytasks/models/Tarefas.java
+++ dailytasks/src/main/java/br/com/dailytasks/models/Tarefas.java
@@ -1,5 +1,6 @@
 package br.com.dailytasks.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 
 @Entity
 @Table(name = "tb_tarefa")
@@ -30,6 +32,13 @@ public class Tarefas {
 	@JoinColumn(name = "usuario_id")
 	@JsonIgnoreProperties("myTasks")
 	private Usuario usuario;
+	 
+	@NotBlank
+	@Size(min = 4, max = 10)
+	private String cor;
+	
+	@Column(name = "finalizada", columnDefinition = "BOOLEAN DEFAULT false")
+	private boolean finalizada;
 
 	public Long getId_tarefa() {
 		return id_tarefa;
@@ -63,5 +72,20 @@ public class Tarefas {
 		this.usuario = usuario;
 	}
 
-	
+	public boolean isFinalizada() {
+		return finalizada;
+	}
+
+	public void setFinalizada(boolean finalizada) {
+		this.finalizada = finalizada;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
 }
